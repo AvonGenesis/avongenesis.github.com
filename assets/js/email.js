dd1c1cdb5506372b77f5de1e78baeacc182968af
Julian Tith <juliantith@fastmail.fm>
2013-11-17 00:04:04 -0800

    Rearrange folders, obfuscate email, background image now local

---
+++ assets/js/email.js
@@ -0,0 +1,9 @@
+var x="function f(x){var i,o=\"\",ol=x.length,l=ol;while(x.charCodeAt(l/13)!" +
+"=48){try{x+=x;l+=l;}catch(e){}}for(i=l-1;i>=0;i--){o+=x.charAt(i);}return o" +
+".substr(0,ol);}f(\")14,\\\"200\\\\010\\\\410\\\\730\\\\720\\\\n\\\\R530\\\\" +
+"r\\\\DLOYBDi720\\\\n\\\\{CH400\\\\OK@Mz>rh;qzq{U7H.w}dfz-.Wgfk)jleneAhn\\\\" +
+"420\\\\010\\\\U?1&\\\"\\\\8&q%=$.'(f730\\\\771\\\\'%MV530\\\\]700\\\\030\\\\"+
+"120\\\\V[SA]AE730\\\\DAK@YHEM\\\"(f};o nruter};))++y(^)i(tAedoCrahc.x(edoCr" +
+"ahCmorf.gnirtS=+o;721=%y;i=+y)14==i(fi{)++i;l<i;0=i(rof;htgnel.x=l,\\\"\\\"" +
+"=o,i rav{)y,x(f noitcnuf\")";
+while(x=eval(x));
